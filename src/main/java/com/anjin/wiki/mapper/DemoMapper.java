834d5ebc2238d279a171a24383ac8ac88d36951d
anjin <2998087078@qq.com>
2021-05-13 17:02:01 +0800

    3-5 集成mybatis generator,生成demo表代码

---
+++ src/main/java/com/anjin/wiki/mapper/DemoMapper.java
@@ -0,0 +1,30 @@
+package com.anjin.wiki.mapper;
+
+import com.anjin.wiki.domain.Demo;
+import com.anjin.wiki.domain.DemoExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface DemoMapper {
+    long countByExample(DemoExample example);
+
+    int deleteByExample(DemoExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Demo record);
+
+    int insertSelective(Demo record);
+
+    List<Demo> selectByExample(DemoExample example);
+
+    Demo selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") Demo record, @Param("example") DemoExample example);
+
+    int updateByExample(@Param("record") Demo record, @Param("example") DemoExample example);
+
+    int updateByPrimaryKeySelective(Demo record);
+
+    int updateByPrimaryKey(Demo record);
+}
