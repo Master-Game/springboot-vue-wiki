cd7b188a2870ed658469bbfeb56d8c84d3b91d7f
anjin <2998087078@qq.com>
2021-05-13 19:10:35 +0800

    3-6 完成电子书列表查询接口

---
+++ src/main/java/com/anjin/wiki/controller/EbookController.java
@@ -0,0 +1,24 @@
+package com.anjin.wiki.controller;
+
+import com.anjin.wiki.domain.Ebook;
+import com.anjin.wiki.service.EbookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
+@RequestMapping("/ebook")
+public class EbookController {
+
+    @Resource
+    private EbookService ebookService;
+    
+    @GetMapping("/list")
+    public List<Ebook> list(){
+        return ebookService.list();
+    }
+
+}
