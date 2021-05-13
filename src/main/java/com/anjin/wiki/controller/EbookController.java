374d050553af8c35b0365de99b0475189ec1db92
anjin <2998087078@qq.com>
2021-05-13 19:25:09 +0800

    3-6 增加通用的返回类CommonResp，以后所有后端接口的返回值全部都是CommonResp类型

--- src/main/java/com/anjin/wiki/controller/EbookController.java
+++ src/main/java/com/anjin/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.anjin.wiki.controller;
 
 import com.anjin.wiki.domain.Ebook;
+import com.anjin.wiki.resp.CommonResp;
 import com.anjin.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,8 +18,11 @@ public class EbookController {
     private EbookService ebookService;
     
     @GetMapping("/list")
-    public List<Ebook> list(){
-        return ebookService.list();
+    public CommonResp list(){
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 
 }
